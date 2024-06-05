1e66227bdd92fab8c78c6f6e46a23a9f9bdde258
bhoomi1030 <bhoomi18p2003@gmail.com>
2024-06-05 13:08:42 +0000

    done

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -4,5 +4,8 @@ var accountPassword = "123456"
 accountCity = "Jaipur"
 
 //accountId = 2
-
+accountEmail = "ac@gmailcom"
+accountPassword = "1237896"
+accountCity = "Ayodhya"
 console.log(accountId)
+console.table([accountId, accountPassword, accountEmail ,accountCity])
---
+++ 01_Basics/02_variables.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version 
+// alert(2+3) we are using node js not browser
+// null = standalone value 
+// boolean = either true or false 
+//number = 2 to power 53
+//bigint( the very large numeric values )
+let age = 23 
+let boolean = true 
+let state = null
+//console.table([age , boolean , state])
+// one more variable which is typeof that tells the type of a variable
+console.log(typeof 23);
