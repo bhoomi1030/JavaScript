f446de74d3cecc9537252ebb2e3efe8f76081ba6
bhoomi1030 <bhoomi18p2003@gmail.com>
2024-06-06 13:19:23 +0000

    done

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log(1>2);
+console.log(4<8);
+console.log(8!=9);
+console.log(9==6);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
