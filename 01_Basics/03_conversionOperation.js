c20ca54b916d7006f9d6af337af679abd4c20cb9
bhoomi1030 <bhoomi18p2003@gmail.com>
2024-06-06 16:56:29 +0000

    done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = true
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+// it will return type number when quotes are not applied and when quotes are implemented then it will return type string.
+
+//"33"= 22.
+//true = 1 , false = 0.
+//"33abc" = NaN
+
+let isLooged = ""
+let booleanisLooged = Boolean(isLooged)
+console.log(booleanisLooged)
+
+// 1 return true , 0 return false.
+//" " return false.
+//" bhoomi " return true 
+
+let value = 4
+let negValue = -value
+console.log(negValue);
+
+console.log(4+4);
+console.log(4-4);
+console.log(4**4);
+console.log(4*4);
+console.log(4/4);
+console.log(4%4);
+
+let str1 = "karan"
+let str2 = " bhoomi"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+console.log("1"+2+2);
