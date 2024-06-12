267791b61573db770c98b398f2e8dfaf91da0975
bhoomi1030 <bhoomi18p2003@gmail.com>
2024-06-12 13:13:53 +0000

    done

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,37 @@
+const score = 300
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(8))
+
+const number = 34.84
+console.log(balance.toPrecision(2));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//-----------------------------------------------------------------------------------------------------------------
+//MATHS
+console.log(Math.abs(-1234567890));
+
+console.log(Math.round(789.9876));
+
+console.log(Math.ceil(34.1));
+
+console.log(Math.floor(9.9))
+
+console.log(Math.max(6,8,9,0));
+
+console.log(Math.min(6,8,9,0));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random() * (max - min + 1)))
+
