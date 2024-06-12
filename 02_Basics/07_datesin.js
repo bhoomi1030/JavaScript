9f319c069cf881f17c42a5770a8d8b9f97b67468
bhoomi1030 <bhoomi18p2003@gmail.com>
2024-06-12 13:05:11 +0000

    done

---
+++ 02_Basics/07_datesin.js
@@ -0,0 +1,23 @@
+/*let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toISOString());
+console.log(myDate.toDateString());
+
+console.log(typeof myDate);
+
+let createdDate = new Date(2023 , 4 , 20)
+console.log(createdDate.toString());
+console.log(createdDate.toDateString());
+console.log(createdDate.toLocaleDateString());
+console.log(createdDate.toLocaleTimeString());
+
+let mycreatedDate = new Date(2023, 4 , 20 , 5, 3)
+console.log(mycreatedDate.toLocaleString());
+console.log(mycreatedDate.toDateString());*/
+
+let MycreatedDate = new Date("10-12-2003")
+console.log(MycreatedDate.toLocaleDateString());
