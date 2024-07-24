24fe4b635a8be9d7c3ada19472e81d3b0ba9324b
bhoomi1030 <bhoomi18p2003@gmail.com>
2024-07-24 04:38:37 +0000

    done

---
+++ 02_Basics/10_object.js
@@ -0,0 +1,24 @@
+
+//object literals 
+const mySym = Symbol("Key1")
+
+
+
+const Jsuser = { 
+    name : "Bhoomi",
+    "full name" : "Bhoomi prajapati",
+    age : 19 , 
+    mySym : "mykey1",
+    email : "bhoomi18p2003@gmail.com",
+    location : "Ayodhya",
+    loggedin : false , 
+    lastloginDays : ["Monday", "Sunday"]
+ }
+ console.log(typeof Jsuser.age);
+ console.log(Jsuser["full name"]);
+ console.log(typeof Jsuser.email);
+ console.log(Jsuser.mySym);
+
+ Jsuser.age = 20
+ //Object.freeze(Jsuser)
+ //console.log(Jsuser);
