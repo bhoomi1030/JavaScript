359fe0b7d85acedf375e5df9b7fa0e1e6c975982
bhoomi1030 <bhoomi18p2003@gmail.com>
2024-07-28 13:29:07 +0000

    done

--- 02_Basics/10_object.js
+++ 02_Basics/10_object.js
@@ -2,16 +2,14 @@
 //object literals 
 const mySym = Symbol("Key1")
 
-
-
 const Jsuser = { 
     name : "Bhoomi",
     "full name" : "Bhoomi prajapati",
-    age : 19 , 
+    age : 19, 
     mySym : "mykey1",
     email : "bhoomi18p2003@gmail.com",
     location : "Ayodhya",
-    loggedin : false , 
+    loggedin : false, 
     lastloginDays : ["Monday", "Sunday"]
  }
  console.log(typeof Jsuser.age);
@@ -21,4 +19,14 @@ const Jsuser = {
 
  Jsuser.age = 20
  //Object.freeze(Jsuser)
- //console.log(Jsuser);
+ console.log(Jsuser);
+
+ Jsuser.greeting = function() {
+ console.log("heloooooooooooooooo");
+ }
+ Jsuser.greetingTwo = function(){
+ console.log(`Hello Meee , ${this.name}`);
+ }
+ console.log(Jsuser.greeting())
+ console.log(Jsuser.greetingTwo());
+
--- 02_Basics/11_object.js
+++ 02_Basics/11_object.js
@@ -1,3 +1,21 @@
 // singleton
-const Tinderuser = new object()
-console.log(Tinderuser)
+const tinderUser = new Object()
+console.log(tinderUser)
+
+const tinderUser1 = {}
+tinderUser1.name = "bhoomi"
+tinderUser1.age = 23
+tinderUser1.inLogged = false
+
+console.log(tinderUser1);
+
+const regularName = {
+    email : "bhoomi@gmail.com",
+    fullName: {
+        userName: {
+            firstName: "bhoomi",
+            lastName: "prajapati"
+        }
+    }
+}
+console.log(regularName.fullName.userName.firstName)
