b2e060a707aebdd7313d86c03ff66033ba54a82d
bhoomi1030 <bhoomi18p2003@gmail.com>
2024-07-29 05:57:22 +0000

    done

--- 02_Basics/11_object.js
+++ 02_Basics/11_object.js
@@ -18,4 +18,12 @@ const regularName = {
         }
     }
 }
-console.log(regularName.fullName.userName.firstName)
+console.log(regularName.fullName.userName.firstName) 
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj3 = Object.assign(obj1 , obj2)
+console.log(obj3);
+
+
+
---
+++ 03_javaScript/practice.js
@@ -0,0 +1,11 @@
+// suppose you have to make a profile of any instagram handle 
+// practice 1
+const profile = {
+    userName: "bhoomi@12", 
+    follower: 345, 
+    following: 234 , 
+    isfollow : true 
+};
+console.log(profile);
+console.log(typeof profile["userName"]);
+console.log(typeof profile["isfollow"]);
