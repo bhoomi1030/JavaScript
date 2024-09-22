f30937a171881e81c8369e7b783c5ca04e8e9878
bhoomi1030 <bhoomi18p2003@gmail.com>
2024-09-22 11:24:15 +0000

    done

---
+++ 02_Basics/12_object.js
@@ -0,0 +1,11 @@
+const course = {
+    coursename : "js in hindi",
+    price : "9999",
+    courseinstructor : "Bhoomi"
+}
+
+//course.price
+
+const {courseinstructor: instructor} = course 
+console.log(instructor);
+
